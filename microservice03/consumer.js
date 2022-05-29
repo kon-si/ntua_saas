41d865f4d01a71a059a288ef5fa9250797f2b926
nickbel7 <nikolas.bellos@gmail.com>
2022-05-29 22:31:55 +0300

    Live update with sse in frontend (without react)

---
+++ microservice03/consumer.js
@@ -0,0 +1,28 @@
+const { kafka, clientId } = require('./broker');
+
+// INITIALIZE A CONSUMER
+const consumer = kafka.consumer({ // NEW CONSUMER
+    groupId: clientId
+})
+
+const consume = async () => {
+
+    await consumer.connect();
+    await consumer.subscribe({ 
+        topic: 'total',
+        fromBeginning: true
+    })
+    await consumer.run({
+        eachMessage: ({ message }) => {
+            console.log(`
+                received message: ${message.value}
+            `);	
+            value = `${message.value}`;
+            // API call url
+            sendEventsToAll();
+        },
+    })
+
+}
+
+module.exports = consume;
