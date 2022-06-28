0d52aa1339e05e75b91994a6342617d55827c898
kon-si <konstantinos.sideris2000@gmail.com>
2022-06-28 22:22:48 +0300

    Updated Importer (with known bug)

---
+++ microservice-importer-flows/config/database.js
@@ -0,0 +1,35 @@
+const path = require('path')
+require("dotenv").config({ path: path.resolve(__dirname, '../.env') });
+const { Sequelize, DataTypes } = require("sequelize");
+
+let db = {};
+
+const { PG_URI } = process.env;
+const sequelize = new Sequelize(PG_URI, { dialect: 'postgres', dialectOptions: { useUTC: false }, timezone: '+02:00' });
+
+db.sequelize = sequelize;
+db.Sequelize = Sequelize;
+
+db.checkConnection = async function() {
+    sequelize.authenticate().then(() => {
+        console.log("Connection to flows database has been established successfully.");
+
+        //import db models
+        const countries = require("../model/countries")(sequelize, DataTypes);
+        db[countries.name] = countries;
+        const physical_flows = require("../model/physical_flows")(sequelize, DataTypes);
+        db[physical_flows.name] = physical_flows;
+        
+        sequelize.sync()
+        .then(() => {
+            console.log("Flows model synchronised with database.");
+        })
+        .catch(err => console.log(err));
+
+    })
+    .catch(err => {
+        console.error("Error connecting to flows database:", err);
+    });
+}
+
+module.exports = db;
